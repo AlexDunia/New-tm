11d2f0807c07068adf6397847e9269bf267420a1
AlexDunia <thealexdunia@gmail.com>
2022-10-30 20:18:39 +0100

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import ol from './assets/Images/img6.png'
+import Asa from './assets/Images/img4.png'
+import Lay from './assets/Images/img1.png'
+import Buju from './assets/Images/img7.png'
+</script>
+
+<template>
+  <header>
+    <div className="container">
+      <div className="navflex">
+        <div className="flogow">
+          <img src="./assets/Images/mctlogo.png" />
+        </div>
+        <div className="navlinks">
+          <ul>
+            <li><RouterLink to="/">Home</RouterLink></li>
+            <li><RouterLink to="/about">Music</RouterLink></li>
+            <li><RouterLink to="/about">Movieszz</RouterLink></li>
+            <li><RouterLink to="/about">Theatre/Comedy</RouterLink></li>
+            <li><RouterLink to="/about">Sports</RouterLink></li>
+            <li><RouterLink to="/about">Contact Us</RouterLink></li>
+          </ul>
+        </div>
+        <div>
+          <button className="btn">
+            <router-link to="/mycart"> My cart </router-link>
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <RouterView :myproducts="products" />
+</template>
+
+<script>
+export default {
+  setup() {
+        const imageUrl = new URL("../assets/images/", import.meta.url).href;
+        return { imageUrl };
+    },
+  data() {
+    return {
+      products: [
+        {
+          Img: ol,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "December 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        },
+        {
+          Img: Asa,
+          "Name": "ASA LIVE IN CONCERT AT 02 ARENA ",
+          "Date": "June 15 @6:30pm",
+          "Description": "Genesis Event Center, Lagos, Nigeria. anywhere you see canpy, just come first ",
+        },
+         {
+          Img:Lay,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "June 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        },
+         {
+          Img: Buju,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "June 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        },
+          {
+          Img: ol,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "June 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        },
+        {
+          Img: Asa,
+          "Name": "ASA LIVE IN CONCERT",
+          "Date": "June 15 @6:30pm",
+          "Description": "Genesis Event Center, Lagos, Nigeria.",
+        },
+         {
+          Img:Lay,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "June 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        },
+         {
+          Img: Buju,
+          Name: "ASA LIVE IN CONCERT",
+          Date: "June 15 @6:30pm",
+          Description: "Genesis Event Center, Lagos, Nigeria.",
+        }
+      ],
+    };
+  },
+};
+</script>
+<style src="@/assets/style.css"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+import Bg from '../assets/Images/crowd.jpg'
+</script>
+
+<template>
+ 
+
+
+      <div class="spacearound">
+        <img src="../assets/Images/fboyb.jpg"/>
+      </div>
+
+      
+   
+        <div className="latest"> <h1> Latest events </h1> </div>
+         
+         <div class="circular"
+       :style="{ backgroundImage: 'url(' + image + ')' }">
+        <div className="t_grids">
+
+
+      <div className="one_e" v-for="(oneticket, i) in myproducts" :key="i"> 
+      <ul> 
+      <li className="mypimage"> <img :src="oneticket.Img"/> </li>
+        <li className="noe"> {{ oneticket.Name }} </li>
+          <div className="toe"> <i class="fa-solid fa-location-dot"> </i>   {{ oneticket.Description }}  </div> 
+        
+         <div className="toe"> <i class="fa-solid fa-calendar-days"></i> {{ oneticket.Date }} </div> 
+       
+        
+        <li className="toe"> {{ oneticket.Price }} </li>
+        <button className="b_t"> Buy Ticket </button> 
+        </ul>
+      </div>
+ 
+      </div>
+       </div>
+    
+   <div className="cdtext"> <h1> 15 </h1> </div> 
+   <div> className="cdtext" <h2> Days </h2> </div> 
+   <div> <h2> TO GO </h2> </div> 
+  
+
+</template>
+
+<script>
+export default {
+props: ['myproducts'],
+data() {
+  return{
+     image: Bg
+  }
+}
+}
+</script>
